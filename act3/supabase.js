122e8bdb9cfdc2cd99a8fdb23bb7f628c391badd
taco323 <guillermobautista721@gmail.com>
2025-11-03 08:33:35 -0600

    act 3

--- act3/supabase.js
+++ act3/supabase.js
@@ -1,3 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+//import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient('https://xyzcompany.supabase.co', 'publishable-or-anon-key')
+const supabaseClient = supabase.createClient('https://qswexfdefjxkpcrxcyos.supabase.co', 
+'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzd2V4ZmRlZmp4a3BjcnhjeW9zIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEyNzc3MzYsImV4cCI6MjA3Njg1MzczNn0.SYBCzJ8no3C-nCP5tCqbMdDl05C_OdHe6ARXmcT574M')
+
+console.log(supabaseClient)
